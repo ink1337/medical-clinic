e3e428be2c2ad9d303a27db0f56cb1465590120c
ink1337 <dominion.vip@gmail.com>
2025-02-23 19:28:58 +0100

    +ErrorMessage class

--- src/main/java/com/medicalclinic/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/medicalclinic/handler/RestResponseEntityExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.medicalclinic.handler;
 
 import com.medicalclinic.exception.ProcessingPatientException;
-import org.springframework.http.HttpHeaders;
+import com.medicalclinic.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+import java.time.Instant;
 
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ProcessingPatientException.class)
-    public ResponseEntity<Object> handleProcessingPatientException(RuntimeException ex, WebRequest request) {
-        String bodyResponse = ex.getMessage();
-        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleProcessingPatientException(ProcessingPatientException ex) {
+        System.out.println(Instant.now());
+        return ErrorMessage.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .timestamp(Instant.now().toEpochMilli())
+                .message(ex.getMessage())
+                .build();
     }
 }
---
+++ src/main/java/com/medicalclinic/model/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.medicalclinic.model;
+
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+public class ErrorMessage {
+    private String message;
+    private HttpStatus status;
+    private long timestamp;
+}
