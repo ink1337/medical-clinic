495d44b31bb411dbc9f81ec1857617f49022858d
Dominik Malina <dominik.malina@stazysta.comarch.pl>
2025-02-25 16:40:46 +0100

    - unused annotation

--- src/main/java/com/medicalclinic/exception/ProcessingPatientException.java
+++ src/main/java/com/medicalclinic/exception/ProcessingPatientException.java
@@ -1,14 +1,5 @@
 package com.medicalclinic.exception;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-
-import java.util.Locale;
-
-@Slf4j
 public class ProcessingPatientException extends RuntimeException {
 
     public ProcessingPatientException(String message) {
