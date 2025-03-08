dcf0561e283d9d7b22a77aa654e88380d92baf35
ink1337 <dominion.vip@gmail.com>
2025-03-08 20:29:31 +0100

    + nie działa ale powinno

--- src/main/java/com/medicalclinic/mapper/DoctorMapper.java
+++ src/main/java/com/medicalclinic/mapper/DoctorMapper.java
@@ -2,22 +2,25 @@ package com.medicalclinic.mapper;
 
 import com.medicalclinic.model.dto.DoctorDTO;
 import com.medicalclinic.model.entity.Doctor;
+import com.medicalclinic.model.entity.Facility;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface DoctorMapper {
 
-    @Mapping(target = "facilities", expression = "java(new java.util.HashSet<>())")
+    //    @Mapping(target = "facilities", expression = "java(new java.util.HashSet<>())")
+    @Mapping(target = "facilities", source = "facilities", qualifiedByName = "mapFacilityNames")
     DoctorDTO toDTO(Doctor source);
 
-//    @Named("mapFacilityNames")
-//    default Set<String> mapFacilityNames(Set<Facility> facilities) {
-//        Set<String> names = new HashSet<>();
-//        for (Facility facility : facilities) {
-//            names.add(facility.getName());
-//        }
-//        return names;
-//
-//    }
+    @Named("mapFacilityNames")
+    default Set<String> mapFacilityNames(Set<Facility> facilities) {
+        return facilities.stream()
+                .map(Facility::getName)
+                .collect(Collectors.toSet());
+    }
 }
--- src/main/java/com/medicalclinic/model/dto/DoctorDTO.java
+++ src/main/java/com/medicalclinic/model/dto/DoctorDTO.java
@@ -1,7 +1,6 @@
 package com.medicalclinic.model.dto;
 
 
-import com.medicalclinic.model.entity.Facility;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,5 +14,5 @@ public class DoctorDTO {
     private String firstName;
     private String lastName;
     private String speciality;
-    private Set<Facility> facilities;
+    private Set<String> facilities;
 }
